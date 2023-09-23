57d66fb573640d27e3060653deac7b4087c12095
scottwesenberg <scowesen@gmail.com>
2023-09-23 18:21:25 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TermProject1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
